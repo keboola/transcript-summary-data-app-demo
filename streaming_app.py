290bcb96a27d30078efd3489fc171aa78a11e673
jordanrburger <jordanrburger@gmail.com>
2024-04-14 17:44:12 -0400

    Update audio_receiver_size in streaming_app.py

--- streaming_app.py
+++ streaming_app.py
@@ -118,7 +118,7 @@ def main():
     webrtc_ctx = webrtc_streamer(
         key="speech-to-text",
         mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=1024,
+        audio_receiver_size=4096,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": False, "audio": True},
     )
