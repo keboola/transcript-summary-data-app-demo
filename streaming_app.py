88c7c6c74efbd262767d94a0b5e560d9f9b80652
pandyandy <144467422+pandyandy@users.noreply.github.com>
2024-04-17 21:20:50 +0300

    Update

--- streaming_app.py
+++ streaming_app.py
@@ -13,20 +13,21 @@ import vertexai.preview.generative_models as generative_models
 from vertexai.generative_models import GenerativeModel, Part, FinishReason
 
 # Assuming your Google Cloud credentials are set up in Streamlit's secrets
-#credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+#credentials_path = ""
 
+#credentials = service_account.Credentials.from_service_account_file(credentials_path)
 client = speech.SpeechClient()
+    #credentials=credentials
 
 image_path = os.path.dirname(os.path.abspath(__file__))
 
-KBC_LOGO = image_path + "/static/keboola_mini.png"
-GC_LOGO = image_path + "/static/google_mini.png"
-
+KBC_LOGO = image_path + "/static/keboola-gemini.png"
+GC_LOGO = image_path + "/static/gemini.png"
 
 def create_or_clear_file(file_path):
     """Create a new empty file or clear an existing file."""
     with open(file_path, 'w') as file:
-        file.truncate()  # Clear the file
+        file.truncate()  
 
 def append_to_file(file_path, data):
     """Append data to a file."""
@@ -49,50 +50,59 @@ def audio_stream_generator(q):
         yield speech.StreamingRecognizeRequest(audio_content=chunk)
 
 def stream_audio(transcript_queue, stop_event, device_index=None):
-    audio_q = queue.Queue(maxsize=10)
+    while not stop_event.is_set():
+        audio_q = queue.Queue(maxsize=10)
+        start_time = time.time()
+    
+        def audio_callback(indata, frames, time, status):
+            if status:
+                print("Audio Input Error:", status, file=sys.stderr)
+            audio_q.put(bytes(indata))
 
-    def audio_callback(indata, frames, time, status):
-        if status:
-            print("Audio Input Error:", status, file=sys.stderr)
-        audio_q.put(bytes(indata))
+        with sd.RawInputStream(callback=audio_callback, dtype='int16', channels=1, samplerate=16000, device=device_index):
+            requests = audio_stream_generator(audio_q)
+            config = speech.RecognitionConfig(
+                encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=16000,
+                language_code='en-US',
+            )
+            streaming_config = speech.StreamingRecognitionConfig(config=config, interim_results=True)
 
-    with sd.RawInputStream(callback=audio_callback, dtype='int16', channels=1, samplerate=16000, device=device_index):
-        requests = audio_stream_generator(audio_q)
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=16000,
-            language_code='en-US',
-        )
-        streaming_config = speech.StreamingRecognitionConfig(config=config, interim_results=True)
+            try:
+                responses = client.streaming_recognize(config=streaming_config, requests=requests)
+                for response in responses:
+                    current_time = time.time()
+                    elapsed_time = current_time - start_time
+                    if elapsed_time > 240:
+                        break
+                    
+                    if response.error.code:
+                        print("Google Speech API Error:", response.error.message)
+                        break
 
-        try:
-            responses = client.streaming_recognize(config=streaming_config, requests=requests)
-            for response in responses:
-                if response.error.code:
-                    print("Google Speech API Error:", response.error.message)
-                    break
+                    for result in response.results:
+                        if result.is_final:
+                            transcript = result.alternatives[0].transcript
+                            transcript_queue.put(transcript)
+                            print("Transcript updated:", transcript)  # Debugging print
 
-                for result in response.results:
-                    if result.is_final:
-                        transcript = result.alternatives[0].transcript
-                        transcript_queue.put(transcript)
-                        print("Transcript updated:", transcript)  # Debugging print
+                    if stop_event.is_set():
+                        print("Stop event triggered.")
+                        break
 
-                if stop_event.is_set():
-                    print("Stop event triggered.")
-                    break
+            except Exception as e:
+                print("Exception during streaming:", e)
+            finally:
+                audio_q.put(None)  # Signal the generator to terminate
 
-        except Exception as e:
-            print("Exception during streaming:", e)
-        finally:
-            audio_q.put(None)  # Signal the generator to terminate
-
+        time.sleep(1)
 
 def generate_summary(content):
-    vertexai.init(project="keboola-ai", location="us-central1")
+    vertexai.init(project="keboola-ai", location="us-central1", 
+                  #credentials=credentials
+                  )
     model = GenerativeModel("gemini-1.5-pro-preview-0409")
-    #model = GenerativeModel("gemini-1.5-pro-latest")
-
+   
     generation_config = {
         "max_output_tokens": 8192,
         "temperature": 1,
@@ -110,7 +120,6 @@ def generate_summary(content):
         {content}
         """,
         generation_config=generation_config,
-        #safety_settings=safety_settings,
         stream=True,
     )
 
@@ -143,31 +152,18 @@ def summary_update(transcript_queue, summary_queue, stop_event, file_path):
                     
                 append_to_file(file_path, summary)
                 summary_queue.put(summary)
-                #st.session_state['summaries'].append(summary)
                 accumulated_text = ""
             time.sleep(30)
-
-#def display_all_summaries():
-   # for summary in st.session_state.summaries:
-    #    st.write(summary)
 
 def main():
     
     logo_html = f'''
-    <div style="display: flex; align-items: center; justify-content: right; font-size: 30px;">
-        <img src="data:image/png;base64,{base64.b64encode(open(KBC_LOGO, "rb").read()).decode()}" style="height: 60px;">
-        <span style="margin: 0 20px;">➕</span>
-        <img src="data:image/png;base64,{base64.b64encode(open(GC_LOGO, "rb").read()).decode()}" style="height: 60px;">
-        <span style="margin: 0 20px;">🟰</span>
-        <span>❤️</span>
+    <div style="display: flex; justify-content: flex-end;">
+        <img src="data:image/png;base64,{base64.b64encode(open(KBC_LOGO, "rb").read()).decode()}" style="height: 100px;">
         <br><br>
     </div>
     '''
     st.markdown(f"{logo_html}", unsafe_allow_html=True)
-    
-    
-   # if 'summaries' not in st.session_state:
-    #    st.session_state['summaries'] = []
     
     st.title("Real-time Speech Recognition and Summary Generation")
     summaries_file_path = 'summaries.txt'
@@ -193,32 +189,25 @@ def main():
         summary_thread = threading.Thread(
             target=summary_update, args=(transcript_queue, summary_queue, stop_event, summaries_file_path))
 
-        # Start the threads
         transcription_thread.start()
         summary_thread.start()
 
-        # Inform user of status
         st.success("Recording and processing started.")
         st.subheader("Summaries")
         st.write_stream(stream_summaries(summary_queue))
 
     if stop_button:
-        # Set the stop event to signal threads to stop
         if transcription_thread and transcription_thread.is_alive():
             stop_event.set()
-
-        # Wait for threads to finish
             transcription_thread.join()
         if summary_thread and summary_thread.is_alive():
             summary_thread.join()
         
-        # Inform user of status
         st.success("Recording and processing stopped.")
         st.subheader("Summaries")
         summaries = read_file(summaries_file_path)
         for summary in summaries:
             st.write(summary)
-       # display_all_summaries()
         
 if __name__ == "__main__":
     main()
